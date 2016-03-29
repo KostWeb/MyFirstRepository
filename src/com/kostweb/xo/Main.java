cf8c33d3e3ec7b9514c13ff394caedc9d4f86842
Konstantin Zhuravlov <kostweb@gmail.com>
2016-03-29 12:37:36 +0300

    third commit

--- src/com/kostweb/xo/Main.java
+++ src/com/kostweb/xo/Main.java
@@ -3,10 +3,6 @@ package com.kostweb.xo;
 import com.kostweb.xo.controller.Game;
 import com.kostweb.xo.view.*;
 
-
-/**
- * Created by kostweb on 3/29/16.
- */
 public class Main {
 
     public static void main(String[] args) {
